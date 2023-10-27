f38a6a9956ef2edef5d45d9620dc3282757ac73d
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-10-27 08:06:15 +0300

    updating lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,6 @@ import prehook
 import posthook
 
 def lambda_handler(event, context):
-    # df_src_list,df_src_titles = prehook.execute_prehook()
     prehook.execute_prehook()
     hook.execute_hook()
     posthook.execute_posthook()
