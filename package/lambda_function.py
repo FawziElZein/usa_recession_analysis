b6ef9ad4c69cbf06720ef9656defa11cf082840f
FawziElZein <56543752+FawziElZein@users.noreply.github.com>
2023-10-27 08:03:55 +0300

    Delete package directory

--- package/lambda_function.py
+++
@@ -1,9 +0,0 @@
-import hook
-import prehook
-import posthook
-
-def lambda_handler(event, context):
-    # df_src_list,df_src_titles = prehook.execute_prehook()
-    prehook.execute_prehook()
-    hook.execute_hook()
-    posthook.execute_posthook()
--- package/main.py
+++
@@ -1,8 +0,0 @@
-import hook
-import prehook
-import posthook
-
-# df_src_list,df_src_titles = prehook.execute_prehook()
-prehook.execute_prehook()
-hook.execute_hook()
-posthook.execute_posthook()
