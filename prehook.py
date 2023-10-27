fc954512cd79d3a48c30487b0b22806aba5635c5
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-10-27 08:48:26 +0300

    adding logger message to the prehook phase

--- prehook.py
+++ prehook.py
@@ -1,18 +1,17 @@
 from pandas_data_handler import return_create_statement_from_df,return_insert_into_sql_statement_from_df,get_online_csv_into_df_list
-from lookups import Logger,ErrorHandling, InputTypes,DestinationDatabase,ETLStep
+from lookups import ErrorHandling, InputTypes,DestinationDatabase,ETLStep
 from database_handler import return_query,execute_query, create_connection, close_connection,return_data_as_df
 from misc_handler import execute_sql_folder,create_insert_sql
-from logging_handler import show_error_message
+from logging_handler import show_error_message,show_logger_message
 import datetime
 import os
 
 
 def execute_prehook(sql_command_directory_path = './SQL_Commands'):
-    log_level = Logger.LOG_LEVEL.value
-    logging.info(log_level)
-    logger = logging.getLogger()
-    logger.setLevel(log_level)
-    logger.info("Executing Pre-Hook")
+
+    logger_string_prefix = ETLStep.HOOK.value
+    logger_string_postfix = "executing prehook"
+    show_logger_message(logger_string_prefix,logger_string_postfix)
 
     try:
         db_session = create_connection()
