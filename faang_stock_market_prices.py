784f81394ccf089cde5b4fd2901695712d98b023
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-10-27 08:55:54 +0300

    adding error handling to get_faang_historical_prices (function that retrieve stock prices from yahoo finance library)

--- faang_stock_market_prices.py
+++ faang_stock_market_prices.py
@@ -2,8 +2,9 @@ from yahoofinancials import YahooFinancials
 import pandas as pd
 from database_handler import execute_query,return_query,parse_date_columns
 from pandas_data_handler import return_create_statement_from_df,return_insert_into_sql_statement_from_df
-from lookups import DestinationDatabase
+from lookups import DestinationDatabase,ErrorHandling
 from datetime import datetime,timedelta
+from logging_handler import show_error_message,show_logger_message
 import pytz
 
 def create_staging_table(db_session,staging_df,schema_name,table_title):
@@ -51,36 +52,39 @@ def convert_local_to_utc(local_datetime):
 def get_faang_historical_prices(db_session,etl_datetime, dst_schema = DestinationDatabase.SCHEMA_NAME.value):
 
     latest_datetime = get_latest_datetime_from_stock_price_table(db_session,dst_schema)
+    try:
+        if not latest_datetime:
+            latest_datetime = convert_local_to_utc(etl_datetime)
 
-    if not latest_datetime:
-        latest_datetime = convert_local_to_utc(etl_datetime)
+        if latest_datetime.weekday() == 5:
+            latest_datetime += timedelta(days=1)
+        
+        latest_date_str = latest_datetime.strftime('%Y-%m-%d')
 
-    if latest_datetime.weekday() == 5:
-        latest_datetime += timedelta(days=1)
-    
-    latest_date_str = latest_datetime.strftime('%Y-%m-%d')
+        end_datetime = convert_local_to_utc(datetime.today())
+        end_date_str = end_datetime.strftime('%Y-%m-%d')
 
-    end_datetime = convert_local_to_utc(datetime.today())
-    end_date_str = end_datetime.strftime('%Y-%m-%d')
+        tickers = ['META','AMZN','AAPL','NFLX','GOOGL']
+        data = {}
+        for ticker in tickers:
+            yahoo_financials = YahooFinancials(ticker)
+            historical_data = yahoo_financials.get_historical_price_data(latest_date_str, end_date_str, "daily")
+            if historical_data[ticker]:
+                data[ticker] = historical_data[ticker]['prices']
+        for ticker, prices in data.items():
+            df = pd.DataFrame(prices)
+            parse_date_columns(df)
+            df = df.drop('date', axis=1).set_index('formatted_date')
+            df['volume'] = df['volume'].astype(float)
+            df.dropna(inplace=True)
+            source = 'yahoo_finance'
+            df_name = ticker + '_stock_price'
+            dst_table = f"stg_{source}_{df_name}"
+            create_staging_table(db_session = db_session,staging_df = df,schema_name =dst_schema,table_title = dst_table)
+            store_into_staging_table(db_session = db_session, staging_df = df, dst_schema = dst_schema ,dst_table = dst_table)
 
-    tickers = ['META','AMZN','AAPL','NFLX','GOOGL']
-    data = {}
-    for ticker in tickers:
-        yahoo_financials = YahooFinancials(ticker)
-        print("getting data for ",ticker)
-        historical_data = yahoo_financials.get_historical_price_data(latest_date_str, end_date_str, "daily")
-        if historical_data[ticker]:
-            data[ticker] = historical_data[ticker]['prices']
-            print(data[ticker])
-    for ticker, prices in data.items():
-        df = pd.DataFrame(prices)
-        parse_date_columns(df)
-        df = df.drop('date', axis=1).set_index('formatted_date')
-        df['volume'] = df['volume'].astype(float)
-        df.dropna(inplace=True)
-        source = 'yahoo_finance'
-        df_name = ticker + '_stock_price'
-        dst_table = f"stg_{source}_{df_name}"
-        create_staging_table(db_session = db_session,staging_df = df,schema_name =dst_schema,table_title = dst_table)
-        store_into_staging_table(db_session = db_session, staging_df = df, dst_schema = dst_schema ,dst_table = dst_table)
-            
+
+    except Exception as e:
+        error_string_prefix = ErrorHandling.GET_YAHOO_FINANCE_STOCK_PRICE_FAILED.value
+        error_string_postfix = str(e)
+        show_error_message(error_string_prefix,error_string_postfix)
