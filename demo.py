2678b63d2c732b66095a0db74a232ea835e884c6
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-10-27 08:16:50 +0300

    adding selenium into requirements, testing logger function

--- demo.py
+++ demo.py
@@ -3,7 +3,10 @@ from database_handler import create_connection
 from datetime import datetime
 from lookups import CHROME_EXECUTOR
 from webscrape import get_usa_webscrapping_data
+import logging
 
-etl_date = datetime(2007,10,23)
-db_session = create_connection()
-get_usa_webscrapping_data(db_session,etl_datetime=etl_date,does_etl_exists=True,chrome_exec_path = CHROME_EXECUTOR.PATH)
+log_level = "INFO"
+logging.info(log_level)
+logger = logging.getLogger()
+logger.setLevel(log_level)
+logger.info("Schedule is running")
