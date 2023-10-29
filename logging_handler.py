9f5092d6977f3d8091ceb9e79c4b82b5f7d956dd
Fawzi ElZein <Fawzi_zein_94@hotmail.com>
2023-10-29 18:30:42 +0200

    renaming logger_file to app.log

--- logging_handler.py
+++ logging_handler.py
@@ -11,7 +11,7 @@ def show_error_message(error_string_prefix,error_string_suffix ):
 
 def show_logger_message(logger_string_prefix,logger_string_postfix):
 
-    logging.basicConfig(filename = "logger_file.log",level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(filename = "app.log",level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     console_handler.setLevel(logging.INFO)
     
     console_handler.setFormatter(formatter)
